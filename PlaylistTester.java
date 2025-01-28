c186b63cec15d120d749b9c7e1f6294ef2a8ff07
Arjun Dhadwal <adhadwal27@students.polytechnic.org>
2025-01-28 10:25:12 -0800

    object creator in playlist tester

--- PlaylistTester.java
+++ PlaylistTester.java
@@ -10,7 +10,7 @@ public class PlaylistTester {
     public static void main(String[] args) {
         System.out.println("Initializing a Playlist...\n");
         //Make your playlist here
-
+        Playlist myMix = new Playlist(null);
         System.out.println("Adding songs to the Playlist...\n");
         /**
          * Add some songs here. Note that the format for adding a Song to a Playlist p
